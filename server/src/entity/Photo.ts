fffed597c31786bb47cd40d35b7cdbfd4589d552
Felix <felix@grenadamarine.com>
2019-05-10 16:29:24 -0400

    Fixing omissions

--- server/src/entity/Photo.ts
+++ server/src/entity/Photo.ts
@@ -1,9 +1,28 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class Photo {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   url: string;
+
+  @Column()
   thumbnailUrl?: string;
+
+  @Column()
   caption: string;
 
+  @CreateDateColumn()
   createdAt: Date;
+
+  @UpdateDateColumn()
   updatedAt: Date;
 }
